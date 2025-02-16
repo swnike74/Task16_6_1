a52ce5a7458ade50c8351ee77d5d9d7cd1c58514
Ведерников Сергей <swnike@mail.ru>
2025-02-16 21:07:51 +0500

    Добавьте файлы проекта.

---
+++ Task16_6_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task16_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
