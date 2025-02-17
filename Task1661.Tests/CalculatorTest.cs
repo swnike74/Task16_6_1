1f8fa67a90541d588cef627f0801c136e17fff16
Ведерников Сергей <swnike@mail.ru>
2025-02-17 23:04:33 +0500

    update 1

---
+++ Task1661.Tests/CalculatorTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1661.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Addition_MustReturnsCorrectValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Addition(10, 220), Is.EqualTo(230));
+        }
+
+        [Test]
+        public void Substraction_MustReturnCorrectValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Subtraction(200, 10) == 190);
+        }
+
+        [Test]
+        public void Multiplication_MustReturnCorrectValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Miltiplication(20, 10) == 200);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Division(200, 10) == 20);
+        }
+
+
+    }
+}
---
+++ Task16_6_1/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1661
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
